436e4913ca9fd1ddb193a1b18d966494af999bd7
jkonel <17853940199jhk@sina.com>
2020-05-21 12:13:40 +0800

    平衡二叉树修改

--- balancedBinTree.c
+++ balancedBinTree.c
@@ -3,9 +3,9 @@
  * @Author: Jkonel
  * @Date: 2020-05-21 10:21:41
  * @LastEditors: jkonel
- * @LastEditTime: 2020-05-21 10:24:07
+ * @LastEditTime: 2020-05-21 12:12:52
  */
 #include <stdio.h>
 #include <stdbool.h>
 #include "balancedBinTree.h"
-#include "binTree.h"
+#include "balancedBinTree.h"
--- balancedBinTree.h
+++ balancedBinTree.h
@@ -3,11 +3,20 @@
  * @Author: Jkonel
  * @Date: 2020-05-21 10:21:52
  * @LastEditors: jkonel
- * @LastEditTime: 2020-05-21 10:23:10
+ * @LastEditTime: 2020-05-21 12:11:38
  */ 
 
 #ifndef _BALANCEDBINTREE_H
 #define _BALANCEDNINTREE_H
+typedef int balancedBinTreeData;
+
+typedef struct balanced_bin_tree
+{
+    balancedBinTreeData a;
+    struct balanced_bin_tree* left;
+    struct balanced_bin_tree* right;
+    int height;
+} balaancedBinTree, pBalabcedBinTree;
 
 
 #endif
